fc75716bcb5511c41dba932f87ab18b09c7eb714
Juan Vitório <juanvitorioclash@gmail.com>
2026-07-28 10:31:32 -0300

    fix: atualiza configuracao do django no manage.py

--- manage.py
+++ manage.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Utilitário de linha de comando do Django para tarefas administrativas."""
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gerencia_armazem_saude.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Não foi possível importar o Django. Verifique se ele está "
+            "instalado e disponível na variável de ambiente PYTHONPATH. "
+            "Você ativou o ambiente virtual?"
         ) from exc
     execute_from_command_line(sys.argv)
 
