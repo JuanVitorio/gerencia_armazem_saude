35312b6800768990d5d8cd75b1d9da31d56885b2
Juan Vitório <juanvitorioclash@gmail.com>
2026-07-28 22:39:44 -0300

    docs: add host pythonanywhere

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-troque-esta-chave-em-producao'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'juanvitoriodev.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
